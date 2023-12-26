beeb48db0e101f6464e3c0cf88d67c2301d1e71b
sam.lee <swjjang7@gmail.com>
2023-12-26 23:25:21 +0900

    fist commit

---
+++ app/src/main/java/com/swjjang7/searchcontentbykakao/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.swjjang7.searchcontentbykakao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
