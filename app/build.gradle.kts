240bceb278c1246a5c1dc965c55be94cd5f11f11
sam.lee <swjjang7@gmail.com>
2023-12-26 23:37:31 +0900

    기본 사용 라이브러리 버전 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.org.jetbrains.kotlin.kapt)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.com.google.dagger.hilt.android)
 }
 
 android {
@@ -16,6 +19,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
     }
 
     buildTypes {
@@ -40,9 +48,29 @@ dependencies {
 
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
+    implementation(libs.appcompat.resources)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+    implementation(libs.gson)
+    implementation(libs.glide)
+    implementation(libs.coroutines)
+    implementation(libs.coroutines.core)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.logging.interceptor)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.activity.ktx)
+    implementation(libs.fragment.ktx)
+    implementation(libs.recyclerview)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/swjjang7/searchcontentbykakao/SearchContentByKakao.kt
@@ -0,0 +1,8 @@
+package com.swjjang7.searchcontentbykakao
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SearchContentByKakao: Application() {
+}
