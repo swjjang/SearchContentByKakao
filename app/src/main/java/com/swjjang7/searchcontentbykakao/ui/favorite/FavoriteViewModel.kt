c1bda9b38325850cd027736120fefbd32ce0c899
sam.lee <swjjang7@gmail.com>
2023-12-30 01:32:07 +0900

    불필요 코드 제거

--- app/src/main/java/com/swjjang7/searchcontentbykakao/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/swjjang7/searchcontentbykakao/ui/favorite/FavoriteViewModel.kt
@@ -1,11 +1,7 @@
 package com.swjjang7.searchcontentbykakao.ui.favorite
 
 import androidx.lifecycle.viewModelScope
-import com.swjjang7.searchcontentbykakao.domain.model.ApiResult
 import com.swjjang7.searchcontentbykakao.domain.model.FavoriteContent
-import com.swjjang7.searchcontentbykakao.domain.model.onError
-import com.swjjang7.searchcontentbykakao.domain.model.onSuccess
-import com.swjjang7.searchcontentbykakao.domain.usecase.GetFavoriteContentsUseCase
 import com.swjjang7.searchcontentbykakao.domain.usecase.RemoveFavoriteContentsUseCase
 import com.swjjang7.searchcontentbykakao.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,13 +9,11 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteViewModel @Inject constructor(
-//    private val getFavoriteContentsUseCase: GetFavoriteContentsUseCase,
     private val removeFavoriteContentsUseCase: RemoveFavoriteContentsUseCase,
 ) : BaseViewModel() {
     private val _contents = MutableStateFlow<List<FavoriteContent>>(emptyList())
@@ -28,26 +22,7 @@ class FavoriteViewModel @Inject constructor(
     private val _favoriteUpdate = MutableSharedFlow<Boolean>()
     val favoriteUpdate: SharedFlow<Boolean> = _favoriteUpdate
 
-    // MainViewModel 에서 처리하도록 변경 됨
-//    private fun fetchFavoriteContents() {
-//        showLoading(true)
-//
-//        viewModelScope.launch(exceptionHandler) {
-//            getFavoriteContentsUseCase().catch {
-//                emit(
-//                    ApiResult.Error.Unknown(it)
-//                )
-//            }.collect { result ->
-//                result.onSuccess {
-//                    showLoading(false)
-//                    _contents.emit(it)
-//                }.onError {
-//                    showLoading(false)
-//                    showToast(it.toString())
-//                }
-//            }
-//        }
-//    }
+    // 데이터 처리는 MainActivity에서 처리
 
     fun removeFavoriteContent(favoriteContent: FavoriteContent) {
         viewModelScope.launch(exceptionHandler) {
